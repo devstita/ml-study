15e0a6ad29d382096253fbeb93ec668a1c48dc2b
Devstita <devstita@gmail.com>
2021-05-22 00:54:13 +0900

    Made cnn work

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ from PIL import Image
 
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
+saved_model_file_name = 'model_save_cnn.dat'
+
+class Flatten(nn.Module):
+    def forward(self, input):
+        return input.view(input.size(0), -1)
+
 
 def draw(*args):
     tensor = args[0]
@@ -30,51 +36,45 @@ def draw(*args):
 command = input('Input your Command: ')
 
 if command == 'n':  # New model
-    batch_size = 16
-    learning_rate = 1e-5
+    batch_size = 64
+    learning_rate = 1e-4
 
     # Read Data from CSV File
 
     # Make Dataset
-    x_train = torch.FloatTensor(np.array(np.load('dataset/train_data.npy')))
+    x_train = torch.FloatTensor(np.array(np.load('dataset/train_data.npy')).reshape((60000, 1, 28, 28)))
     y_train = torch.FloatTensor(np.array(np.load('dataset/train_label.npy')))
-
-    print(x_train.shape)
-    print(x_train)
 
     train_dataset = TensorDataset(x_train, y_train)
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
-    print('Training Data Load Done !')
+    print('Training Data Load Done!')
 
     # Generate Model and Training
-    # Todo: Check model structure
-    model = nn.Sequential(
-        nn.Sequential(
+    conv1 = nn.Sequential(
             nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        ), nn.Sequential(
+            nn.MaxPool2d(kernel_size=2)
+    )
+    conv2 = nn.Sequential(
             nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        ), nn.Sequential(
-            nn.Linear(7 * 7 * 64, 10, bias=True)
-        )
+            nn.MaxPool2d(kernel_size=2),
+            Flatten()
+    )
+    fc = nn.Sequential(nn.Linear(7 * 7 * 64, 50, bias=True),
+                       nn.ReLU(),
+                       nn.Linear(50, 10, bias=True)
     )
 
+    model = nn.Sequential(conv1, conv2, fc)
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-    print('Model Generation Done !')
+    print('Model Generation Done!')
 
-    epochs = 40
+    epochs = 2
     for epoch in range(epochs + 1):
         for X, Y in train_dataloader:
-            print(X)
-            print(X.shape)
-            print(Y)
-            print(Y.shape)
-
             prediction = model(X)
             cost = functional.mse_loss(prediction, Y)
 
@@ -84,33 +84,30 @@ if command == 'n':  # New model
 
             print(f'Epoch: {epoch}/{epochs}\tCost: {cost.item()}')
 
-    torch.save(model, 'model_save.dat')
-elif command == 'la':  # Load model
-    if not os.path.isfile('model_saved.dat'):
+    torch.save(model, saved_model_file_name)
+elif command == 'la':  # Load model and Check Accuracy
+    if not os.path.isfile(saved_model_file_name):
         print('Saved model does not exist..')
         exit(3)
 
     print('Model Exist!!')
     print('... Loading Model ...')
 
-    model = torch.load('model_save.dat')
+    model = torch.load(saved_model_file_name)
     model.eval()
 
-    x_test = torch.FloatTensor(np.array(np.load('dataset/test_data.npy')))
+    x_test = torch.FloatTensor(np.array(np.load('dataset/test_data.npy').reshape((10000, 1, 28, 28))))
     y_test = torch.FloatTensor(np.array(np.load('dataset/test_label.npy')))
 
     data_size = len(y_test)
     count_of_true = 0
     for idx, in_data in enumerate(x_test):
-        prediction = torch.argmax(model(in_data))
+        draw(in_data)
+        prediction = torch.argmax(model(in_data.reshape(1, 1, 28, 28)))
         real_data = torch.argmax(y_test[idx])
         print(f'Prediction: {prediction}, y_test[idx]: {real_data}')
-        if prediction == real_data:
-            count_of_true += 1
-            print('Correct:', count_of_true)
-        else:
-            print('Wrong   :', count_of_true)
 
+    # Todo: Find why the accuracy is too low
     print('Accuracy: ' + str((count_of_true / data_size) * 100))
 elif command == 'lq':
     file_path = input('File Path: ')
@@ -118,14 +115,14 @@ elif command == 'lq':
         print('File does not exist..')
         exit(2)
 
-    if not os.path.isfile('model_save.dat'):
+    if not os.path.isfile(saved_model_file_name):
         print('Saved model does not exist..')
         exit(3)
 
     print('Model Exist!!')
     print('... Loading Model ...')
 
-    model = torch.load('model_save.dat')
+    model = torch.load(saved_model_file_name)
     model.eval()
 
     read_data = np.array(Image.open(file_path).convert('L'), dtype=np.float32).reshape(784)
